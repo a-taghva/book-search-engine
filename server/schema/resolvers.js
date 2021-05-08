457f9acbde7b813fa55c31d2978a3b07d897d76e
a-taghva <taghvali99@gmail.com>
2021-05-08 18:20:15 -0400

    addUser mutation

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -36,7 +36,12 @@ const resolvers = {
       return { token, user };
     },
 
-    
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
   }
 };
 
