2ed256984aef2bb3e2c58b7741ad3fd79551a179
a-taghva <taghvali99@gmail.com>
2021-05-08 17:50:36 -0400

    me query

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -1,9 +1,18 @@
 const { User} = require('../models')
+const { AuthenticationError } = require('apollo-server-express');
 
 const resolvers = {
   Query: {
-    me: async () => {
-      return User.find()
+    me: async (parent, params, context) => {
+      if (!context.user) {
+        return new AuthenticationError("You are not logged in!!");
+      }
+
+      const userData = await User.findOne({ _id: context.user._id }) 
+        .select('-__v -password')
+        .populate('savedBooks');
+
+      return userData;
     }
   }
 };
--- server/server.js
+++ server/server.js
@@ -6,13 +6,15 @@ const routes = require('./routes');
 const { ApolloServer } = require('apollo-server-express');
 
 const { typeDefs, resolvers } = require('./schema');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 })
 
 server.applyMiddleware({ app });
