b58eef3b65869dc5364c4302a12224b38e7297b1
a-taghva <taghvali99@gmail.com>
2021-05-08 23:17:52 -0400

    implement LOGIN_USER mutation

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,13 +2,18 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
+// import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
+
+import { LOGIN_USER } from '../utils/mutations';
+import { useMutation } from '@apollo/react-hooks';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+
+  const [loginUser, { error }] = useMutation(LOGIN_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -26,13 +31,18 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+    //   const response = await loginUser(userFormData);
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+    //   if (!response.ok) {
+    //     throw new Error('something went wrong!');
+    //   }
 
-      const { token, user } = await response.json();
+      // const { token, user } = await response.json();
+      const { email, password } = userFormData;
+      const { token, user } = await loginUser({
+        variables: { email, password }
+      })
+
       console.log(user);
       Auth.login(token);
     } catch (err) {
